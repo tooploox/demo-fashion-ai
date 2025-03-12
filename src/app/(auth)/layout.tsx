de7011a106afb95c6f408129cc3f9ee4a546efa0
Krystian Fras <krystian@fras.me>
2025-03-12 15:31:01 +0100

    Add bg

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,14 @@ export default function AuthLayout({
   return (
     <div className="relative min-h-svh flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
+        <div
+          className="absolute inset-0 bg-zinc-900"
+          style={{
+            backgroundImage: "url('/auth_bg.jpeg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
         <div className="relative z-20 flex items-center text-lg font-medium">
           <Logo />
         </div>

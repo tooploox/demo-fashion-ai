f246902f5de8ca4d9766a8259e5a3e860448eb37
Krystian Fras <krystian@fras.me>
2025-03-20 15:19:59 +0100

    add user link

--- src/app/(dashboard)/Topbar.tsx
+++ src/app/(dashboard)/Topbar.tsx
@@ -4,16 +4,28 @@ import { Logo } from "@/components/Logo";
 import { Button } from "@/components/ui/button";
 import { useUser } from "@stackframe/stack";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export function Topbar() {
   const user = useUser();
+  const [isAdmin, setIsAdmin] = useState(false);
 
+  useEffect(() => {
+    async function checkAdmin() {
+      const teams = await user?.listTeams();
+      if (teams?.find((team) => team.displayName === "sales")) {
+        setIsAdmin(true);
+      }
+    }
+    checkAdmin();
+  }, [user, user?.selectedTeam]);
   return (
     <header className="sticky top-0 flex h-16 w-full shrink-0 items-center gap-2 border-b bg-background px-4">
       <Link href="/">
         <Logo />
       </Link>
       <div className="ml-auto flex items-center gap-4">
+        {isAdmin && <Link href="/add-user">Add user</Link>}
         <p className="text-sm font-medium">
           Tokens: {user?.clientReadOnlyMetadata?.tokens}
         </p>
